fd03a36e5bb0672b17c4cd65a05fb98f65592369
Irany Luis-Gomez <iluisgomez8920@floridapoly.edu>
2025-11-12 21:14:11 +0000

    Computer Input

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
 print("Welcome to Rock-Paper-Scissors!")
+
+# Computers Output
+import random
+choices = ["Scissors", "Rock", "Paper"]
+comp_choice = random.choice(choices)
+
+print(f"Computer chose: {comp_choice}")
